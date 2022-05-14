8870f5a279886a97b10e3817c91bef5b72e5479d
TROUVERIE Joachim <jtrouverie@joakode.fr>
2022-05-14 10:23:17 +0200

    fix: framework name

--- {{ cookiecutter.project_slug }}/static/{{ cookiecutter.svelte_directory }}/src/pages/Index.svelte
+++ {{ cookiecutter.project_slug }}/static/{{ cookiecutter.svelte_directory }}/src/pages/Index.svelte
@@ -1,11 +1,12 @@
 <script lang="ts">
+  let frameworkName = 'Svelte';
 </script>
 
 <div class="content">
   <img class="logo" src="https://flask.palletsprojects.com/en/2.0.x/_static/flask-icon.png" alt="Flask logo">
   <img class="logo" src="https://avatars.githubusercontent.com/u/47703742?s=200&v=4" alt="Inertia logo">
-  <img class="logo" src="https://github.com/sveltejs/branding/blob/master/svelte-logo.png" alt="Svelte logo">
-  <h1 class="title">Welcome to your Flask-Inertia Vue app.</h1>
+  <img class="logo" src="https://svelte.dev/svelte-logo.svg" alt="Svelte logo">
+  <h1 class="title">Welcome to your Flask-Inertia {frameworkName} app.</h1>
   <p class="info">
     For more information please visit the
     <a href="https://flask-inertia.readthedocs.io/">documentation</a>
